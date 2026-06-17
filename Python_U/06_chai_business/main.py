9d81071cad736e6261d1b9985096cca868736d69
Raunak Raj Jaiswal <raunakrajjaiswal.com>
2026-06-17 20:41:01 +0530

    pro

---
+++ Python_U/06_chai_business/main.py
@@ -0,0 +1,12 @@
+# import recipes.flavors
+
+# print(recipes.flavors.ginger_chai())
+
+
+# from recipes.flavors import elachai_name, ginger_chai
+
+# print(ginger_chai())
+
+
+
+from .recipes.flavors import ginger_chai
---
+++ Python_U/06_chai_business/recipes/flavors.py
@@ -0,0 +1,5 @@
+def elachai_name():
+    return "Elaichai chai is ready"
+
+def ginger_chai():
+    return "Ginger chai ready"
