a65ec5172286a74221806e78f496d6c454ec11f8
Raunak Raj Jaiswal <raunakrajjaiswal.com>
2026-06-21 18:53:57 +0530

    decor

---
+++ Python_U/09_decorators/01_basics.py
@@ -0,0 +1,49 @@
+
+# def my_decorator(func):
+
+
+#     def wrapper():
+#         print("Before function runs")
+#         func()
+#         print("After function runs")
+#     return wrapper
+    
+
+# @my_decorator
+# def greet():
+#     print("Hello from decorator class from chaicode")
+
+
+# greet()
+# print(greet.__name__)
+
+
+
+
+
+
+
+
+
+
+
+
+
+from functools import wraps
+def my_decorator(func):
+
+    @wraps(func)
+    def wrapper():
+        print("Before function runs")
+        func()
+        print("After function runs")
+    return wrapper
+    
+
+@my_decorator
+def greet():
+    print("Hello from decorator class from chaicode")
+
+
+greet()
+print(greet.__name__)
---
+++ Python_U/09_decorators/03_auth_decorator.py
@@ -0,0 +1,20 @@
+from functools import wraps
+
+
+def require_admin(func):
+    @wraps(func)
+    def wrapper(user_role):
+        if user_role != "admin":
+            print("Access denied! only admin")
+        else:
+            return func(user_role)
+    return wrapper
+
+
+@require_admin
+def access_tea_inventory(role):
+    print("Access granted to tea inventory")
+
+
+access_tea_inventory("user")
+access_tea_inventory("admin")
